08f68fcea20b83cb5d6d7a5cbee4441eaec0872f
aboodeh abooodeh <abdsabagh8@gmail.com>
2022-06-30 19:28:26 +0200

    scaffold added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text("Hello World"),
+  const image = "https://www.w3schools.com/w3css/img_lights.jpg";
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[700],
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          "I Am Rich",
+        )),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(image),
+        ),
+      ),
     ),
   ));
 }
