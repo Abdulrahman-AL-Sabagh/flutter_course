ffd5ba3a71b4bdbf9e1e5f861c7c52d911dc6109
aboodeh abooodeh <abdsabagh8@gmail.com>
2022-07-04 22:44:38 +0200

    adding states to the dice app

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +15,24 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void generateRandomValue() {
+    setState(() {
+      rightDiceNumber = (Random().nextInt(6) + 1);
+      leftDiceNumber = (Random().nextInt(6) + 1);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,9 +42,9 @@ class DicePage extends StatelessWidget {
               child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextButton(
-              onPressed: () {},
+              onPressed: generateRandomValue,
               child: Image.asset(
-                "images/dice1.png",
+                "images/dice$leftDiceNumber.png",
               ),
             ),
           )),
@@ -34,9 +53,9 @@ class DicePage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: TextButton(
               focusNode: null,
-              onPressed: () {},
+              onPressed: generateRandomValue,
               child: Image.asset(
-                "images/dice2.png",
+                "images/dice$rightDiceNumber.png",
               ),
             ),
           ))
