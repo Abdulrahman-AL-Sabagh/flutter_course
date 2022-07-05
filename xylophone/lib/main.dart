167b2f73ed74bc295e60f24bf550ffc86b1c3a82
aboodeh abooodeh <abdsabagh8@gmail.com>
2022-07-05 15:34:16 +0200

    finalizing the app

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(XylophoneApp());
-}
+void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   final player = AudioPlayer();
@@ -15,39 +13,35 @@ class XylophoneApp extends StatelessWidget {
     );
   }
 
-  Widget buildKey(int num, Color color) {
-    return Expanded(
-      child: Container(
-        color: color,
-        child: TextButton(
-          onPressed: () {
-            playSound(num);
-          },
-          child: const Text(""),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              buildKey(1, Colors.red),
-              buildKey(2, Colors.orange),
-              buildKey(3, Colors.yellow),
-              buildKey(4, Colors.blue),
-              buildKey(5, Colors.green),
-              buildKey(6, Colors.pink),
-              buildKey(7, Colors.purple),
-            ],
+  Expanded buildKey(int num, Color color) => Expanded(
+        child: Container(
+          color: color,
+          child: TextButton(
+            onPressed: () {
+              playSound(num);
+            },
+            child: const Text(""),
           ),
         ),
-      ),
-    );
-  }
+      );
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildKey(1, Colors.red),
+                buildKey(2, Colors.orange),
+                buildKey(3, Colors.yellow),
+                buildKey(4, Colors.blue),
+                buildKey(5, Colors.green),
+                buildKey(6, Colors.pink),
+                buildKey(7, Colors.purple),
+              ],
+            ),
+          ),
+        ),
+      );
 }
